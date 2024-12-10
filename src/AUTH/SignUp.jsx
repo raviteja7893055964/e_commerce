4aa88e51024b73cff73a5d37b97de5306b4b6d83
Ponnam Raviteja <ponnamraviteja789@gmail.com>
2024-12-10 09:11:43 +0530

    8th
    8th

--- src/AUTH/SignUp.jsx
+++ src/AUTH/SignUp.jsx
@@ -12,7 +12,7 @@ function SignUp() {
     const handleSignup = (e) => {
         e.preventDefault();
         axios
-            .post("http://localhost:5000/signup", { name, email, password })
+            .post("https://roaring-sunshine-d3a623.netlify.app//signup", { name, email, password })
             .then((result) => {
                 console.log(result);
                 navigate("/");
